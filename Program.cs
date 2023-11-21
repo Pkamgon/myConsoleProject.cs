66f464dd731bf4478812a5a5f5c6dc70cd4a1b00
Pkamgon <Pkamgon@gmail.com>
2023-11-21 16:00:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("What is your name?");
+        string name = Console.WriteLine();
+        Console.WriteLine("Hello, " + name + "!");
+        Console.Read();
+        }
+        
+    }
+}
